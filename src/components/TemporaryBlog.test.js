7ed0f3e7d43daa9b599a5f490c8812711bc74cbd
Andrea Mogren <andrea.mogren91@gmail.com>
2019-12-15 16:45:46 +0100

    5.13 and 5.14 complete

--- src/components/TemporaryBlog.test.js
+++ src/components/TemporaryBlog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
 import TemporaryBlog from './TemporaryBlog'
 
 test('renders content', () => {
@@ -13,13 +13,31 @@ test('renders content', () => {
     const component = render(
         <TemporaryBlog blog={blogContent}/>
     )
-
+    
     const contentDiv = component.container.querySelector('.content')
     const likesDiv = component.container.querySelector('.likes')
 
-    console.log(likesDiv)
-
     expect(contentDiv).toHaveTextContent("It's almost Christmas!")
     expect(contentDiv).toHaveTextContent("Santa")
-    expect(likesDiv).tohaveTextContent(1)
+    expect(likesDiv).toHaveTextContent(1)
+})
+
+test('clicking the button twice calls the event handler twice', () => {
+    const blogContent = {
+        title: "It's almost Christmas!", 
+        author: "Santa",
+        likes: 1,
+    }
+
+    const mockHandler = jest.fn()
+
+    const { getByText } = render(
+        <TemporaryBlog blog={blogContent} onClick={mockHandler}/>
+    )
+    
+    const button = getByText('like')
+    fireEvent.click(button)
+    fireEvent.click(button)
+
+    expect(mockHandler.mock.calls.length).toBe(2)
 })
